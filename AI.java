2293355ce78b3fb99ba1fc6830e7a795d191e2c9
neal <neal@162.247.88.167>
2017-02-06 22:51:22 -0500

    add player class; change line 28 in gofish -- Player to human

--- AI.java
+++ AI.java
@@ -2,4 +2,14 @@ class AI extends Player {
       public AI() {
           
       }
+
+    @Override
+    public void getReply() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void reply() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 }
--- Human.java
+++ Human.java
@@ -2,4 +2,14 @@ class Human extends Player {
       public Human() {
           
       }
+
+    @Override
+    public void getReply() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void reply() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 }
